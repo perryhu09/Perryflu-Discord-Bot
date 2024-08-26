08bfe08744cd5d56160189e94a67e52c5e5ed14e
perryhu <dominichx001@gmail.com>
2024-08-25 22:44:11 -0400

    modified /helpme menu

--- cogs/help_cog.py
+++ cogs/help_cog.py
@@ -14,12 +14,16 @@ class HelpCog(commands.Cog):
         embed = discord.Embed(
             title = "Perryflu Help Page",
             description = """
-            ### Commands
+            ## Commands
             - **/helpme** - Show this help page
             - **/timer {seconds}** - sets a timer for specified number of seconds
+            - **/userinfo** - Get information on a specific user
+            - **/meme** - fetch random meme from r/ProgrammingHumour
             - **/ping** - Get the ping of the bot
             - **/latency** - Details on latency of bot
-            - **/userinfo** - Get information on a specific user
+            - **/balance** - [eco] display your balance
+            - **/work** - [eco] earn some cash
+            - **/beg** - [eco] try begging for money
             """,
             color = EMBEDS_COLOR
         )
